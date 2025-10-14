1e3bedece852d19097431afc5372c9b60c7ada8d
nachjv19 <nachjvasquez@gmail.com>
2025-10-14 17:46:48 -0500

    delete cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import authRoutes from './routes/auth.routes';
@@ -10,7 +9,6 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(helmet());
-app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cookieParser());
