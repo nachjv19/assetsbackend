b61467bf9629856b3af2566df5349dd22bddc6e9
nachjv19 <nachjvasquez@gmail.com>
2025-10-14 21:01:28 -0500

    Update auth.controller.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,7 +3,6 @@ import UserModel from '../models/User';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import { logger } from '../utils/logger';
 import { AuthRequest } from '../middlewares/auth.middlewares';
 
 dotenv.config();
